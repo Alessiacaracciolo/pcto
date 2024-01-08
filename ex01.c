b2200ed92763b94af69412fb001bc734224b190c
Alessiacaracciolo <155971233+Alessiacaracciolo@users.noreply.github.com>
2024-01-08 15:10:01 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () 
+{
+    int intero= 1; 
+    
+    printf("intero: %d\n", intero); 
+    return 0;
+
+    int A = 2;
+    printf("intero: %d\n", intero);
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () 
+{
+    float o = 28;
+    printf("float: %f\n", o);
+    return 0; 
+
+    char e = 'E';
+    printf("char: %c\n", e);
+    return 0;
+
+    char l = 'L';
+    printf("char: %c\n", l);
+    return 0;
+
+    int q = 11;
+    printf("int: %c\n", q);
+    return 0;
+}
+
